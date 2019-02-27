5c3f8ce311835c8da2b61d866b73eb560e7a3abe
zepher2211 <zepher2211@gmail.com>
2019-02-27 11:11:36 -0600

    It finally works

--- book.rb
+++ book.rb
@@ -1,1 +1,33 @@
+require_relative './author.rb'
 
+class Book
+
+
+  attr_accessor :title, :author, :word_count
+
+  @@all = []
+
+  def initialize()
+    @title = title
+    @author = author
+    @word_count = word_count
+  end
+
+  def self.all
+    Author.books
+  end
+
+
+  def author
+    @author
+  end
+
+  def title
+    @title
+  end
+
+  def word_count
+    @word_count
+  end
+
+end

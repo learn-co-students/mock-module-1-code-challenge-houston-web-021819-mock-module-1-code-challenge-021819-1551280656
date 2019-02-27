c93b4ea27f4c3d139b28400e4daa910d66aea319
github username <github email address>
2019-02-27 11:00:13 -0600

    complete

--- author.rb
+++ author.rb
@@ -1,1 +1,46 @@
+class Author
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialze(name)
+        @name = name
+        @@all << self
+        @books = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books(book)
+        @books << book
+        book.author = self
+    end
+
+    def write_book(book)
+        book = Book.new()
+    end
+
+    def total_words
+        total_words = 0
+        Book.word_count.each do | word_count |
+            total_words += word_count
+        end
+    end
+
+    def most_words
+        most_words = nil
+        Book.word_count.each do | word_count |
+            if most_words = nil || most_words < word_count
+                most_words = word_count
+            end
+        end
+    end
+
+end
+
+author = Author.new("Name")
+
 
--- book.rb
+++ book.rb
@@ -1,1 +1,35 @@
+class Book
+
+    attr_accessor :title, :word_count, :author
+
+    @@all = []
+
+    def initialize(title, word_count)
+        @title = title
+        @word_count = word_count
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author(author)
+        author = Author.all.find do | author |
+            author.book == self
+        end
+    end
+
+    def title
+        @title
+    end
+
+    def word_count
+        @word_count
+    end
+
+end
+
+book = Book.new("book", 1000)
+book2 = Book.new("book2", 2000)
 

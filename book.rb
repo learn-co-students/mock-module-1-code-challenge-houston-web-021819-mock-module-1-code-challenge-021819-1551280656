548e22ab4801ccc8d3f652c1608be06d76d55528
Thomas_Hansen <ctrhansen@yahoo.com>
2019-02-27 11:18:24 -0600

    Done

--- book.rb
+++ book.rb
@@ -1,1 +1,23 @@
+class Book
 
+    attr_accessor :book, :author, :title, :word_count
+
+    def initialize(book, author, title, word_count)
+        @book = book
+        @author = author
+        @title = title
+        @word_count = word_count
+    end
+
+end
+
+#- `Book.all`
+#should return all of the books
+#- `Book#author`
+#should return the author instance who wrote this book
+#- `Book#title`
+#should return the title of the book
+#- `Book#word_count`
+#should return the number of words in the book
+
+#`ruby console.rb`

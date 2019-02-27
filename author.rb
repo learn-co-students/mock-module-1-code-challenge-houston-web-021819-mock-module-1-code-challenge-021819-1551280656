9a6daaa8c5b8033a508d1d8e451870ffe580edc9
moranja <jeffadammoran@gmail.com>
2019-02-27 10:08:57 -0600

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,43 @@
+class Author
+  @@all = []
+  attr_accessor :name
 
+  def initialize(name)
+    self.name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select do |novel|
+      novel.author == self
+    end
+  end
+
+  def write_book (title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    word_count_sum = 0
+    self.books.each do |authors_books|
+      word_count_sum += authors_books.word_count
+    end
+    word_count_sum
+  end
+
+  def self.most_words
+    leading_author = nil
+    leader = 0
+    self.all.each do |author|
+      if author.total_words > leader
+        leader = author.total_words
+        leading_author = author
+      end
+    end
+    leading_author
+  end
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,20 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+gabe_habash = Author.new('Gabe Habash')
+andrew_sean_greer = Author.new('Andrew Sean Greer')
+james_baldwin = Author.new('James Baldwin')
+ofeigur_sigurdsson = Author.new('Ofeigur Sigurdsson')
+cixin_liu = Author.new('Cixin Liu')
+
+stephen_florida = gabe_habash.write_book('Stephen Florida', 28700)
+fire_next_time = james_baldwin.write_book('The Fire Next Time', 10600)
+giovanni = james_baldwin.write_book('Giovanni\'s Room', 15900)
+oraefi = ofeigur_sigurdsson.write_book('Oraefi: The Wasteland', 40000)
+three_body_problem = cixin_liu.write_book('The Three-Body Problem', 40000)
+dark_forest = cixin_liu.write_book('The Dark Forest', 51200)
+deaths_end = cixin_liu.write_book('Death\'s End', 60400)
+less = andrew_sean_greer.write_book('Less', 27300)
+
 binding.pry
-
-
+gabe_habash

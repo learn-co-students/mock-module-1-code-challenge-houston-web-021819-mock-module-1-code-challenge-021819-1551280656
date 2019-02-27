58ac8d22bd8b20701b8fe84d90ae11b330dcfd2f
github username <github email address>
2019-02-27 11:09:10 -0600

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,39 @@
+class Author
+attr_accessor :name :book
 
+@@all = []
+
+  def initialize(name:, book:)
+    self.name = name
+    self.book = book
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    authors_books = []
+    Author.select do |Auth|
+    authors_books << author.books
+    end
+  authors_books
+  end
+
+  def write_book(title, word_count)
+    Book.new(title, word_count, self)
+  end
+
+  def total_words
+    authors_books = self.books
+    word_count = authors_books.all.select do |word_count|
+    find_word_total = word_count.each { |a| sum+=a }
+    end
+  return find_word_total
+  end
+
+  def most_words
+    self.all.total_words.max
+  end
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,31 @@
+class Book
+attr_accessor :name :author
 
+@@all = []
+
+
+  def initialize(name:, author:)
+    self.name = name
+    self.author = author
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author
+    self.all.select do |book|
+      author.book == self
+  end
+
+  def title
+    name.book == self
+  end
+
+  def word_count
+    self.all.select do |book|
+    word_count.book == self
+    end
+  end
+end

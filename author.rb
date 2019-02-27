91094d5203ce64d3dc08f5577002ed8bd0dadea2
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-02-27 10:48:04 -0600

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+require 'pry'
+require_relative './book.rb'
+class Author
+    attr_accessor :name, :book, :title, :words
+    @@all = []
+ def initialize(name, book)
+    self.name = name
+    self.book = book
+    @@all << self
+ end
+ @@books = []
+ def books(books)
+    @@books << self.book
+ end
 
+ def self.all
+  @@all.name 
+ end
+ def write_book(title, words)
+    Book.new 
+ end
+ @@total_words = []
+ 
+ def total_words
+    @@total_words<< @@books.length
+    @@total_words
+ end
+
+ def self.most_words
+   most_words = @@total_words.max
+   most_words
+    
+ end
+
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,17 @@
+require 'pry'
+require_relative './author.rb'
+class Book
+    attr_accessor :title, :author,  :word_count
+    @@all = []
+    def initialize(title, author, word_count)
+       self.title = title
+       self.author = author 
+       self.word_count = word_count
+       @@all << self
+    end
 
+    def self.all
+     @@all
+    end
+
+end

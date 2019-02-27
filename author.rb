5a171e30425ecf7748fe01135b17f30b1b7b894b
github username <github email address>
2019-02-27 10:22:14 -0600

    mock challenge tasks completed

--- author.rb
+++ author.rb
@@ -1,1 +1,45 @@
 
+class Author
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select do |book|
+      book.author == self
+    end
+  end
+
+  def write_book(title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    sum_words = 0
+    self.books.select do |book|
+      sum_words = sum_words + book.word_count
+    end
+    sum_words
+  end
+
+  def self.most_words
+    word_sums = (
+      @@all.map do |author|
+        author.total_words
+      end )
+    @@all.find do |author|
+      author.total_words == word_sums.max
+    end
+  end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,32 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+reynolds = Author.new("Alister Reynolds")
+banks = Author.new("Ian M. Banks")
+asher = Author.new("Neal Asher")
+hamilton = Author.new("Peter F. Hamilton")
+
+revelation = reynolds.write_book("Revelation Space", 91000)
+redemption = reynolds.write_book("Redemption Ark", 88000)
+absolution = reynolds.write_book("Absoluteion Gap", 108000)
+consider = banks.write_book("Consider Phlebus", 135000)
+player = banks.write_book("The Player Of Ganes", 79000)
+weapons = banks.write_book("Use of Weapons", 100000)
+sonata = banks.write_book("The Hydrogen Sonata", 184000)
+skinner = asher.write_book("The Skinner", 87000)
+technician = asher.write_book("The Technician", 99000)
+intelligence = asher.write_book("Dark Intelligence", 102000)
+factory = asher.write_book("War Factory", 108000)
+infinity = asher.write_book("Infinity Engine", 109000)
+dysfunction = hamilton.write_book("The Reality Dysfunction", 300000)
+alchemist = hamilton.write_book("The Neutronium Alchemist", 330000)
+naked = hamilton.write_book("The Naked God", 350000)
+pandora = hamilton.write_book("Pandora's Star", 250000)
+judas = hamilton.write_book("Judas Unchained", 291000)
+
+
 binding.pry
 
 
+puts "All done!"
